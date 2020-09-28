64e7b3c867ed0b820a7fe37a0aa3bd39ea5f494a
AymenCheb <chebbi.aymen@hotmail.com>
2020-09-28 23:14:06 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,7 +15,7 @@ def anagrams(words: list = None) -> bool:
         words = []
         first_word = Input("Entrer le premier mot: ")
         second_word = Input("Entrer le second mot: ")
-        
+        first
         pass
 
     return False
